c38c855ed2536b0a8516ce4eb6ae34d2d8c69e65
Sule Arslan <sule.arslan@student.evbtraining.de>
2024-10-17 00:06:28 +0200

    ToDoList Feature/toString-Methode

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-         ToDoList td1 =new ToDoList("Frühstück für Kinder machen", false,1 );
-        System.out.println();
+         ToDoList tdl =new ToDoList("Frühstück für Kinder machen", false,1 );
+        System.out.println(tdl);
     }
 }
--- src/ToDoList.java
+++ src/ToDoList.java
@@ -6,6 +6,17 @@ public class ToDoList {
     LocalDateTime ldt = LocalDateTime.now();
     boolean erledigt;
     int prio;
+
+    @Override
+    public String toString() {
+        return "ToDoList{" +
+                "aufgabe='" + aufgabe + '\'' +
+                ", ldt=" + ldt +
+                ", erledigt=" + erledigt +
+                ", prio=" + prio +
+                '}';
+    }
+
     public ToDoList(String aufgabe, boolean erledigt, int prio) {
         this.aufgabe = aufgabe;
         this.erledigt = erledigt;
