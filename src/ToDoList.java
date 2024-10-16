9c44b3f985e9502b1f0be5a7f86eb7cbae1c8f24
Sule Arslan <sule.arslan@student.evbtraining.de>
2024-10-16 16:12:40 +0200

    classe ToDoList ausgearbeitet

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,2 +1,13 @@
+import java.time.LocalDateTime;
+
 public class ToDoList {
+    String aufgabe;
+    LocalDateTime ldt = LocalDateTime.now();
+    boolean erledigt;
+    int prio;
+    public ToDoList(String aufgabe, boolean erledigt, int prio) {
+        this.aufgabe = aufgabe;
+        this.erledigt = erledigt;
+        this.prio = prio;
+    }
 }
